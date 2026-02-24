111a767361c20c479859f2479e05bda966eeef58
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-25 02:48:52 +0300

    Added Jest tests for getCardValue function

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,27 @@ const getCardValue = require("../implement/3-get-card-value");
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
+test(`Should return 10 when given a face card`, () => {
+  expect(getCardValue("J♦")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♣")).toEqual(10);
+});
+test(`Should return the numeric value when given a number card`, () => {
+  expect(getCardValue("2♠")).toEqual(2);
+  expect(getCardValue("3♥")).toEqual(3);
+  expect(getCardValue("4♦")).toEqual(4);
+  expect(getCardValue("5♣")).toEqual(5);
+  expect(getCardValue("6♠")).toEqual(6);
+  expect(getCardValue("7♥")).toEqual(7);
+  expect(getCardValue("8♦")).toEqual(8);
+  expect(getCardValue("9♣")).toEqual(9);
+  expect(getCardValue("10♠")).toEqual(10);
+});
+test ('Should return invalid card error when given invalid cards', () => {
+  expect(() => getCardValue("1♠")).toThrow();
+  expect(() => getCardValue("11♥")).toThrow();
+});
+
 
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
