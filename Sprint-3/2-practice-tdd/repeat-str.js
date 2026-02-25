03b7a1155ee59f06f3d3d4dac6956fe108981349
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-25 03:31:59 +0300

    Implemented repeatStr function

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,8 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str,count) {
+  if (count < 0) {
+    throw new Error("Count must be a non-negative");
+  }
+  return str.repeat(count);
 }
 
 module.exports = repeatStr;
