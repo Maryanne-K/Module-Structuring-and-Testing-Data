2f1339f4293a60bb73aea58926d7476817009c56
Maryanne Mosonik <chmosonik@gmail.com>
2026-02-25 03:17:06 +0300

    Refactor getOrdinalNumber function to correctly return ordinal suffixes based on input number

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const str = String(num);
+
+  const lastTwo = num % 100;
+
+  if (lastTwo === 11 || lastTwo === 12 || lastTwo === 13) {
+    return str + "th";
+  }
+ const lastDigit = num % 10;
+if (lastDigit === 1) {
+    return str + "st";
+  } else if (lastDigit === 2) {
+    return str + "nd";
+  } else if (lastDigit === 3) {
+    return str + "rd";
+  } else {
+    return str + "th";
+  }
+
+
 }
 
 module.exports = getOrdinalNumber;
